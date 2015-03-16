48c69e85e279fcd262afb28de4900017fdd56672
Craig Tracey <craigtracey@gmail.com>
2015-03-16 15:50:25 -0400

    Remove the system's setuptools
    If we are installing setuptools from pip, there is no need for it to
    be present from the operating system.

--- scripts/prepare_debian.sh
+++ scripts/prepare_debian.sh
@@ -4,7 +4,7 @@ apt-get update
 apt-get install -y build-essential ruby1.9.1-dev git python-dev python-virtualenv libxml2-dev libxslt-dev libffi-dev libmysqlclient-dev libpq-dev libsqlite3-dev wget
 
 # remove pip if present and get the latest
-apt-get remove -y python-pip
+apt-get remove -y python-pip python-setuptools
 GET_PIP_MD5=add41078298d8111714c6b87636714f5
 wget https://bootstrap.pypa.io/get-pip.py 
 if md5sum get-pip.py | grep -q $GET_PIP_MD5; then 
