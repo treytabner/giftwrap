a4b17655595b0f2b89bd63edbc4ccb635bb4d816
Craig Tracey <craigtracey@gmail.com>
2015-03-16 14:55:02 -0400

    Use the latest pip and setuptools
    We should always be using the latest/very recent pip and setuptools
    for giftwrap. At the same time, we should be careful to install from
    the Internet, so check get-pip.py's hash before we install.

--- scripts/prepare_debian.sh
+++ scripts/prepare_debian.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y build-essential ruby1.9.1-dev git python-pip python-dev python-virtualenv libxml2-dev libxslt-dev libffi-dev libmysqlclient-dev libpq-dev libsqlite3-dev
+apt-get install -y build-essential ruby1.9.1-dev git python-dev python-virtualenv libxml2-dev libxslt-dev libffi-dev libmysqlclient-dev libpq-dev libsqlite3-dev wget
+
+# remove pip if present and get the latest
+apt-get remove -y python-pip
+GET_PIP_MD5=add41078298d8111714c6b87636714f5
+wget https://bootstrap.pypa.io/get-pip.py 
+if md5sum get-pip.py | grep -q $GET_PIP_MD5; then 
+    python get-pip.py
+fi
+pip install -U setuptools swiftly
