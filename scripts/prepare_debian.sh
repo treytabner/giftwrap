b8a2b0223f0359a83c95fc13fa3fe979d5359efe
Craig Tracey <craigtracey@gmail.com>
2015-03-17 17:56:11 -0400

    Update Vagrantfile to make use of OpenStack provider
    Updating the Vagrantfile so that builds may be run in an OpenStack
    cleanroom instance.  More to come on this one.

--- scripts/prepare_debian.sh
+++ scripts/prepare_debian.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-apt-get update
+apt-get dist-upgrade -y
+apt-get update 
+sleep 15
 apt-get install -y build-essential ruby1.9.1-dev git python-pip python-dev python-virtualenv libxml2-dev libxslt-dev libffi-dev libmysqlclient-dev libpq-dev libsqlite3-dev
